33d6dea7c901690201e12aa8fb02c9c0606b4b14
pradeepmajji853 <pradeepmajji853@gmail.com>
2024-09-16 23:29:21 +0530

    vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 
 export default defineConfig({
-  base: '/cbitccc/',  
-  build: {
-    outDir: 'dist',  
-  }
-});
+  plugins: [react()],
+})
